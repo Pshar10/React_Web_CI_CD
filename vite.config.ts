a77ec8ae0cff04a6464f85156fd45d050d81e895
Pranav <pranavsharma.89406@gmail.com>
2025-07-02 19:53:39 +0200

    added gh pages pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React_Web_CI_CD/', // 👈 required for GitHub Pages
+  base: '/',  // changed from '/React_Web_CI_CD/' to '/' for custom domain root
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
